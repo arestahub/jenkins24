978bbdc01d3231ab1d59c27a42607d554aa75670
arestahub <83729290+arestahub@users.noreply.github.com>
2024-04-17 10:51:44 +0200

    Add files via upload

---
+++ src/main/java/Mi_grupo/Mi_Artifact/MiArtifactApplication.java
@@ -0,0 +1,13 @@
+package Mi_grupo.Mi_Artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Mi_grupo/Mi_Artifact/MiArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package Mi_grupo.Mi_Artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
